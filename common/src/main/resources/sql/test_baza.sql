a254e14fd02b6e0e82461a0e3dbf7836f5800a80
luszczyk <lukasz@luszczyk.net>
2012-10-25 00:02:11 +0200

    implementing new concept of view

---
+++ common/src/main/resources/sql/test_baza.sql
@@ -0,0 +1,30 @@
+﻿-- Table: md
+
+DROP TABLE md;
+
+CREATE TABLE md
+(
+  id serial NOT NULL,
+  name character varying(255) NOT NULL,
+  data oid NOT NULL,
+  createdate date NOT NULL,
+  ext character varying(20) NOT NULL,
+  CONSTRAINT md_pkey PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+
+-- Table: image
+
+DROP TABLE image;
+
+CREATE TABLE image
+(
+  name character varying(255) NOT NULL,
+  data oid NOT NULL,
+  day date
+)
+WITH (
+  OIDS=FALSE
+);
---
+++ web/src/main/webapp/WEB-INF/pages/res.jsp
@@ -0,0 +1,4 @@
+<%@ page import="net.luszczyk.mdbv.common.model.DataBase" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<%=session.getAttribute("res")%>
